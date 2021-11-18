408a2085cba131540fa893321761100965824237
osmaug <anar.osmanov.one@gmail.com>
2021-11-18 20:42:50 +0400

    second lesson homework - initial file

--- reactus/src/index.js
+++ reactus/src/index.js
@@ -14,7 +14,9 @@ const age = 15;
 const FunctionComponent = ({ test2 }) => {
   return (
     <div>
-      <h3 onClick={test2}>Heading</h3>
+      <h3 onClick={test2} id="one">
+        Heading
+      </h3>
       <h1>Hello Reactus 2</h1>
       <h1>Age = {age}</h1>
     </div>
@@ -33,22 +35,23 @@ class ClassComponent extends React.Component {
   }
 }
 
-const App = ({ test1, ...rest }) => {
+function App({ test1, ...rest }) {
   return (
     <div>
       <h1>{test1}</h1>
+      <isBefore />
       <FunctionComponent {...rest} />
       <hr />
       <ClassComponent {...rest} />
     </div>
   );
-};
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App
       test1={1}
-      test2={() => document.write("here i am")}
+      test2={() => (document.getElementById("one").innerHTML = "here i am")}
       test3={() => console.log("test 3")}
     />
   </React.StrictMode>,
